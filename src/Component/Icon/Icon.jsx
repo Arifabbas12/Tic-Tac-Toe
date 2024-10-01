f65892479d3fc3c0bc05937211fe06cae47a0842
Arif Abbas <abbasarif647@gmail.com>
2024-10-02 01:14:26 +0530

    tic-tac-28-sep

---
+++ src/Component/Icon/Icon.jsx
@@ -0,0 +1,13 @@
+import { FaRegCircle, FaTimes,FaPen } from 'react-icons/fa';
+
+ function Icons({ name }){
+     if(name == "circle"){
+        return <FaRegCircle />;
+     }else if(name == "cross"){
+        return <FaTimes />;
+    }else{
+        return <FaPen />;
+    }
+}
+
+export default Icons;
